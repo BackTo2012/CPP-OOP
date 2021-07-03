d1f057919f342f3b0e4289625fe7e4faf704dd5a
BackTo2012 <1742331636@qq.com>
2021-07-03 18:15:31 +0800

    C++

--- 5-Section 12/2-List模版类--带析构函数 -List与Point类组装.cpp
+++ 5-Section 12/2-List模版类--带析构函数 -List与Point类组装.cpp
@@ -39,23 +39,63 @@ void List<T>::travalList()const {
 
 
 
+template <typename T>
+List<T>::~List() {
+    cout << "~List is called!  Its sizeof(ListNode<T>) is " << sizeof(ListNode<T>) << ", sizeof(T) is " << sizeof(T) << endl;
+    ListNode<T>* x, *psave;
+    x = Head;
+    while (x != NULL)
+    {
+        psave = x->link;
+        cout << "The data " << x->data << " will be deleted!" << endl;
+        delete x;
+        x = psave;
+    }
+    Head = NULL;
+}
+template <typename T>
+void List<T>::append(T val) {
+    ListNode<T>* p = new ListNode<T>;
+    p->data = val;
+    p->link = NULL;
+    if (Head == NULL)
+        Head = p;
+    else
+    {
+        ListNode<T>* tmpp = Head;
+        while (tmpp->link != NULL) {
+            tmpp = tmpp->link;
+        }
+        tmpp->link = p;
+    }
+}   // 链尾增加一个元素
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+class Point {
+    int x, y;
+public:
+    Point(int x = 0, int y = 0): x(x), y(y) { //cout<<"this="<<this<<" ";
+        cout << " Point(" << x << "," << y << ") is called" << endl;
+    }
+    Point(const Point  &p): x(p.x), y(p.y) { //cout<<"this="<<this<<" ";
+        cout << "Copy constructor Point(" << p << ") is called" << endl;
+    }
+    Point & operator = (const Point  &p) {//cout<<"this="<<this<<" "; cout<<"&p="<<&p<<" ";
+        cout << "Point's operator =(" << p << ") is called" << endl;
+        x = p.x; y = p.y; return *this;
+    }
+    ~Point() {//cout<<"this="<<this<<" ";
+        cout << "~Point(" << x << "," << y << ") is called" << endl;
+    }
+    friend ostream & operator << (ostream &, Point const & p);
+};
+ostream & operator << (ostream &out, Point const & p) {
+    out << "(" << p.x << "," << p.y << ")";
+    return out;
+}
 //StudybarCommentBegin
 int main(void)
 {
