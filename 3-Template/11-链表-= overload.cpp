464dafd5304a686c6e476f1f9404adeab2942010
BackTo2012 <1742331636@qq.com>
2021-07-03 16:58:18 +0800

    C++

---
+++ 3-Template/11-链表-= overload.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+using namespace std;
+template <typename T>
+class Node
+{
+public:
+	T data;
+	Node* next;
+};
+template <class T>
+class List
+{
+	Node<T> *head, *tail;
+public:
+	List() {
+		head = NULL;
+		tail = NULL;
+	}
+	void push_back(const T& x) {
+		Node<T> *newhead = new Node<T>;
+		newhead->data = x;
+		newhead->next = NULL;
+		if (head == NULL)
+		{
+			head = newhead;
+			tail = newhead;
+		}
+		else
+		{
+			tail->next = newhead;
+			tail = newhead;
+		}
+	}
+	T& operator[](int i) {
+		int Index = 0;
+		Node<T> *temp = head;
+		while (temp->next != NULL)
+		{
+			if (Index == i)
+				return temp->data;
+			Index++;
+			temp = temp->next;
+		}
+		return temp->data;
+	}
+	List(const Node<T>& z) {
+		head = z.head;
+		tail = z.tail;
+	}
+	List<T>& operator=(const List<T>& x) {
+		if (x.head == NULL)
+		{
+			head = NULL;
+			tail = NULL;
+			return *this;
+		}
+		else
+		{
+			head = NULL;
+			tail = NULL;
+			Node<T>*temp = x.head;
+			while (temp->next != NULL) {
+				push_back(temp->data);
+				temp = temp->next;
+			}
+			push_back(temp->data);
+			return *this;
+		}
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+//StudybarCommentBegin
+
+int main() {
+	int n;
+	cin >> n;
+	List<int> list;
+	int i;
+	for (i = 0; i < n; i++) {
+		list.push_back(i);
+	}
+	List<int> list2(list);
+	list2.push_back(123456);
+	cin >> n;
+	cout << list2[n - 2] << " " << list2[n];
+	list = list2;
+	list2[0] = n * 97;
+	list[0] = n * 79;
+	cout << " " << list[0] << " " << list2[0] << " " << list[n];
+}
+//StudybarCommentEnd
---
+++ 3-Template/12-链表- 分析输出.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+using namespace std;
+template <typename T>
+class Node
+{
+public:
+	T data;
+	Node* next;
+	Node() {next = NULL;};
+	Node(const T& x): data(x) {next = NULL;};
+};
+template <class T>
+class List
+{
+	Node<T> *head, *tail;
+public:
+	List() {
+		head = new Node<T>(0);
+		tail = head;
+		// head = NULL;
+		// tail = NULL;
+	}
+	~List() {
+		if (head->next != NULL)
+		{
+			Node<T>* temp = head->next;
+			while (temp != NULL)
+			{
+				Node<T>* tmpp = temp;
+				temp = temp->next;
+				delete tmpp;
+			}
+		}
+		delete head;
+	}
+	void push_back(const T& x) {
+		Node<T> *newhead = new Node<T>;
+		newhead->data = x;
+		newhead->next = NULL;
+		if (head == NULL)
+		{
+			head = newhead;
+			tail = newhead;
+		}
+		else
+		{
+			tail->next = newhead;
+			tail = newhead;
+		}
+	}
+	T& operator[](int i) {
+		int Index = 0;
+		Node<T> *temp = head;
+		while (temp->next != NULL)
+		{
+			if (Index == i)
+				return temp->data;
+			Index++;
+			temp = temp->next;
+		}
+		return temp->data;
+	}
+	List(const List<T>& z) {
+		head = z.head;
+		tail = z.tail;
+		// if (head == NULL)
+		// {
+		// 	head = NULL;
+		// 	tail = NULL;
+		// }
+		// else
+		// {
+		// 	head = NULL;
+		// 	tail = NULL;
+		// 	Node<T>*temp = head;
+		// 	while (temp->next != NULL) {
+		// 		push_back(temp->data);
+		// 		temp = temp->next;
+		// 	}
+		// 	push_back(temp->data);
+		// }
+	}
+	List<T>& operator=(const List<T>& x) {
+		if (x.head == NULL)
+		{
+			head = NULL;
+			tail = NULL;
+			return *this;
+		}
+		else
+		{
+			head = NULL;
+			tail = NULL;
+			Node<T>*temp = x.head;
+			while (temp->next != NULL) {
+				push_back(temp->data);
+				temp = temp->next;
+			}
+			push_back(temp->data);
+			return *this;
+		}
+	}
+};
+class Point {
+	int x, y;
+public:
+	Point(int xx = 0, int yy = 0) {
+		x = xx;
+		y = yy;
+		cout << "\nPoint(x=0,y=0 ) is called(" << x << "," << y << ")!";
+	}
+	Point(const Point & p) {
+		x = p.x;
+		y = p.y;
+		cout << "\nPoint(const Point & p) is called(" << x << "," << y << ")!";
+	}
+	~Point() {
+		cout << "\n~Point is called(" << x << "," << y << ")!";
+	}
+	friend ostream &operator<<(ostream &output, const Point &P);
+};
+ostream &operator<<(ostream &out, const Point &p) {
+	out << "(" << p.x << "," << p.y << ")";
+	return out;
+}
+
+
+//StudybarCommentBegin
+
+void fun()
+{
+	List<Point> plist;
+	cout << "\n after constructor";
+	plist.push_back(Point(1, 1));
+	plist.push_back(Point(2, 2));
+	List<Point> plist2(plist);
+	cout << "\n after copy constructor";
+	cout << plist2[1];
+	plist2.push_back(Point(3, 3));
+	plist2.push_back(Point(5, 5));
+	plist = plist2;
+	cout << "\n after = overload";
+	cout << plist[3];
+}
+
+
+int main() {
+	fun();
+	cout << "\nFigure out the output of this program!";
+	return 0;
+}
+//StudybarCommentEnd
